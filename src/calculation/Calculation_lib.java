d761cd59853aa475df58c3c90dd367f71b377535
ts719soccer@gmail.com <ts719soccer@gmail.com>
2016-11-01 15:49:22 +0900

    20161024 submit

---
+++ src/calculation/Calculation_lib.java
@@ -0,0 +1,34 @@
+package calculation;
+
+import java.util.Scanner;
+
+public class Calculation_lib {
+	
+	private int m,n;
+	
+	public Calculation_lib(int m, int n){
+		this.m=m;
+		this.n=n;
+	}
+	
+	public int getMinus(){
+		return m-n;
+		
+	}
+	public int getProduct(){
+		return m*n;
+	}	
+	public int getDivide(){
+		return m/n;
+	}
+
+	public int getMod(){
+		
+		return m%n;
+	}	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
